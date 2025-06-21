bc9743084e2b412403f109fe3abe80956cca7a3a
rupak1005 <rupak1005saini@gmail.com>
2025-06-21 15:08:59 +0530

    Refactor loader and main page structure; enhance animations and styles for improved user experience

--- css/finalProject/script.js
+++ css/finalProject/script.js
@@ -1,13 +1,40 @@
-gsap.from(".line h1",{
-    y:150,
-    stagger:0.1,
-    duration:0.7
+var tl = gsap.timeline();
+tl.from(".line h1", {
+  y: 150,
+  stagger: 0.25,
+  duration: 0.6,
+  delay: 0.5,
+});
+tl.from("#line1-part1", {
+  opacity: 0,
+  onStart: function () {
+    var h5timer = document.querySelector("#line1-part1 h5");
+    var grow = 0;
+    setInterval(function () {
+      if (grow < 100) {
+        h5timer.innerHTML = grow++;
+      } else {
+        h5timer.innerHTML = grow;
+      }
+    }, 33);
+  },
+});
+tl.to('.line h2',{
+    animationName:"anime",
+    opacity:1
 })
-var count=document.querySelector("#countline")
-var counter=0;
-setInterval(()=>{
-    if(counter<100){
-    count.textContent=counter++}else{
-        count.textContent=100;
-    }
-},30)
+tl.to("#loader", {
+  opacity: 0,
+  duration: 0.2,
+  delay: 4,
+});
+tl.from("#page1",{
+    delay:0.2,
+    y:1600,
+    opacity:0,
+    duration:0.5,
+    ease:Power4
+})
+tl.to("#loader",{
+    display:"none"
+})
